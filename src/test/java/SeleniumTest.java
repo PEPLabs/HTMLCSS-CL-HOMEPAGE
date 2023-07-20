84b1f6d8627644d23f5873323461c2fc2f9f85bc
LiquidPlummer <kyle.plummer@revature.com>
2023-07-20 13:00:59 -0400

    added selenium testing stuff, no tests yet

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+
+public class SeleniumTest {
+
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        // Set up ChromeDriver path
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+
+        // Get file
+        File file = new File("intro.html");
+        String path = "file://" + file.getAbsolutePath();
+
+        // Create a new ChromeDriver instance
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        webDriver = new ChromeDriver(options);
+
+        // Open the HTML file
+        webDriver.get(path);
+    }
+
+    @After
+    void teardown() {
+        webDriver.quit();
+    }
+
+    /*
+    TODO: Write tests
+     */
+
+}
